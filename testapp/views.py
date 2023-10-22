8645e4401e7813c229563d88e5a307837457fb0a
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-22 15:39:07 +0900

    sakujyo

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from testapp import app
 import cv2
 import os
+import shutil
 # ultralyticsのYOLOモジュールをインポート
 from ultralytics import YOLO
 
@@ -12,6 +13,39 @@ model = YOLO(model="yolov8n-seg.pt")
 import threading
 
 processing_done = False
+
+def sakujyo():
+
+    # カレントディレクトリのファイルとフォルダの一覧を取得
+    items = os.listdir("./testapp/static")
+    print("jiji")
+    print(items)
+    
+    # 削除したいフォルダのパスを指定
+    folder = 'testapp/static/down'
+
+    # フォルダが存在するかどうかをチェック
+    if os.path.exists(folder):
+        # フォルダが存在する場合は、shutil.rmtreeで削除
+        shutil.rmtree(folder)
+        # 削除したことを表示
+        print('Folder deleted: ' + folder)
+    else:
+        # フォルダが存在しない場合は、何もしない
+        print('Folder does not exist: ' + folder)
+
+    # 削除したいファイルのパターンを指定（.gitignore以外）
+    pattern = '.gitignore'
+
+    # testapp/static/upの中のファイルを取得
+    files = os.listdir('testapp/static/up')
+
+    # パターンにマッチするファイルを削除
+    for file in files:
+        if file != pattern:
+            os.remove('testapp/static/up/' + file)
+            # 削除したことを表示
+            print('File deleted: ' + file)
 
 # 画像処理を行う関数を定義
 def process_image(filename):
@@ -26,6 +60,10 @@ def process_image(filename):
     # カレントディレクトリのファイルとフォルダの一覧を取得
     items = os.listdir("./testapp/static")
     print(items)
+    
+def done_false():
+    global processing_done
+    processing_done = False
 
 # ルートディレクトリにアクセスしたときの処理
 @app.route('/')
@@ -36,6 +74,8 @@ def index():
 # /uploadにPOSTリクエストが送られたときの処理
 @app.route('/upload', methods=['POST'])
 def upload():
+    done_false()
+    sakujyo()
     # リクエストからファイルを取得
     file = request.files['file']
     # ファイルが存在しない場合はエラーメッセージを表示
@@ -54,8 +94,6 @@ def upload():
     print(items)
     
     while not processing_done: pass
-    
-    #processing_done = False
 
     # processed.htmlを表示し、加工前と加工後の画像を表示（加工後の画像はまだ生成されていない可能性がある）
     return render_template('htmls/processed.html', original=filename, processed=filename)
