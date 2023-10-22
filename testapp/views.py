396b92519d8769794cb5675c91115947770eefcd
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-23 02:11:58 +0900

    kaetemita

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request
+from flask import make_response, after_this_request
 from testapp import app
 import cv2
 import os
@@ -6,15 +7,15 @@ import shutil
 # ultralyticsのYOLOモジュールをインポート
 from ultralytics import YOLO
 
-# threadingモジュールをインポート
-import threading
+    # YOLOv8のセグメンテーションモデルをロード
+model = YOLO(model="yolov8n-seg.pt")
 
 #画像処理を行う関数を定義
 def process_image(filename):
-    # YOLOv8のセグメンテーションモデルをロード
-    model = YOLO(model="yolov8n-seg.pt")
-    #画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
+    # 画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
     model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
+    # 画像ファイルのパスを返す
+    return os.path.join('testapp/static/down', filename)
 
 # ルートディレクトリにアクセスしたときの処理
 @app.route('/')
@@ -34,15 +35,21 @@ def upload():
     filename = file.filename
     # ファイルを保存
     file.save('testapp/static/up/' + filename)
-    # 画像処理を別のスレッドで実行する（非同期）
-    #model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
-    thread = threading.Thread(target=process_image, args=(filename,))
-    thread.start()
-    # 画像処理を別のスレッドで実行する（非同期）
-    #model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
     
-    # testapp/static/down/filenameというパスを作る
-    file_path = os.path.join('testapp/static/down', filename)
-    while True:
-        if os.path.exists(file_path):
-            return render_template('htmls/processed.html', original=filename, processed=filename)
+    # レスポンスオブジェクトを作成
+    response = make_response(render_template('htmls/processed.html', original=filename, processed=filename))
+    
+    # 画像処理を登録
+    @after_this_request
+    def process_image_after(response):
+        # 画像処理を実行し、ファイルパスを取得
+        file_path = process_image(filename)
+        # ファイルパスからファイル名だけ取り出す
+        file_name = os.path.basename(file_path)
+        # レスポンスオブジェクトにファイル名を追加する（任意）
+        response.headers['X-Processed-File'] = file_name
+        # レスポンスオブジェクトを返す
+        return response
+    
+    # レスポンスオブジェクトを返す
+    return response
