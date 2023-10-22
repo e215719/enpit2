91c729a172b62de2b97240b259a2669b25760eb6
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-22 13:32:38 +0900

    thread

--- testapp/views.py
+++ testapp/views.py
@@ -8,6 +8,16 @@ from ultralytics import YOLO
 # YOLOv8のセグメンテーションモデルをロード
 model = YOLO(model="yolov8n-seg.pt")
 
+# threadingモジュールをインポート
+import threading
+
+# 画像処理を行う関数を定義
+def process_image(filename):
+    # 画像を読み込む
+    img = cv2.imread('testapp/static/up/' + filename)
+    # 画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
+    result = model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down")
+
 # ルートディレクトリにアクセスしたときの処理
 @app.route('/')
 def index():
@@ -26,17 +36,8 @@ def upload():
     filename = file.filename
     # ファイルを保存
     file.save('testapp/static/up/' + filename)
-    # 画像を読み込む
-    img = cv2.imread('testapp/static/up/' + filename)
-    # 画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
-    result = model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down") # projectとnameで保存先を指定
-    # 加工後の画像のファイル名を取得（保存先はtestapp/static/up/）
-    #processed_filename = result[0]['save_path'].split('/')[-1]
-    # 加工後の画像を保存
-    #cv2.imwrite('testapp/static/up/processed_' + filename, model.predict(img))
-    # 加工後の画像のファイル名を取得（保存先はtestapp/static/up/）
-    # processed_filename = result[0]['save_path'].split('/')[-1]
-    # processed.htmlを表示し、加工前と加工後の画像を表示
+    # 画像処理を別のスレッドで実行する（非同期）
+    thread = threading.Thread(target=process_image, args=(filename,))
+    thread.start()
+    # processed.htmlを表示し、加工前と加工後の画像を表示（加工後の画像はまだ生成されていない可能性がある）
     return render_template('htmls/processed.html', original=filename, processed=filename)
-    #return render_template('htmls/processed.html', original=filename, processed='processed_' + filename)
-    #return render_template('htmls/processed.html', original=filename, processed=processed_filename)
