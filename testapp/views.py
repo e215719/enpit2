a21c911452a28f81da71f69c1ad2b62c600c852e
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-23 02:35:30 +0900

    show

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,4 @@
-from flask import render_template, request
-from flask import make_response, after_this_request
+from flask import render_template, request, redirect, url_for
 from testapp import app
 import cv2
 import os
@@ -7,15 +6,18 @@ import shutil
 # ultralyticsのYOLOモジュールをインポート
 from ultralytics import YOLO
 
-    # YOLOv8のセグメンテーションモデルをロード
+# YOLOv8のセグメンテーションモデルをロード
 model = YOLO(model="yolov8n-seg.pt")
 
-#画像処理を行う関数を定義
+# threadingモジュールをインポート
+import threading
+
+# 画像処理を行う関数を定義
 def process_image(filename):
+    # 画像を読み込む
+    #cv2.imread('testapp/static/up/' + filename)
     # 画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
     model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
-    # 画像ファイルのパスを返す
-    return os.path.join('testapp/static/down', filename)
 
 # ルートディレクトリにアクセスしたときの処理
 @app.route('/')
@@ -35,21 +37,20 @@ def upload():
     filename = file.filename
     # ファイルを保存
     file.save('testapp/static/up/' + filename)
-    
-    # レスポンスオブジェクトを作成
-    response = make_response(render_template('htmls/processed.html', original=filename, processed=filename))
-    
-    # 画像処理を登録
-    @after_this_request
-    def process_image_after(response):
-        # 画像処理を実行し、ファイルパスを取得
-        file_path = process_image(filename)
-        # ファイルパスからファイル名だけ取り出す
-        file_name = os.path.basename(file_path)
-        # レスポンスオブジェクトにファイル名を追加する（任意）
-        response.headers['X-Processed-File'] = file_name
-        # レスポンスオブジェクトを返す
-        return response
-    
-    # レスポンスオブジェクトを返す
-    return response
+    # 画像処理を別のスレッドで実行する（非同期）
+    thread = threading.Thread(target=process_image, args=(filename,))
+    thread.start()
+    # /showにリダイレクトする
+    return redirect(url_for('show', filename=filename))
+
+# /showにGETリクエストが送られたときの処理
+@app.route('/show/<filename>')
+def show(filename):
+    # 画像処理が終わったかどうかを確認する
+    file_path = os.path.join('testapp/static/down', filename)
+    if os.path.exists(file_path):
+        # 画像処理が終わっていれば、processed.htmlを表示
+        return render_template('htmls/processed.html', original=filename, processed=filename)
+    else:
+        # 画像処理が終わっていなければ、待機メッセージを表示
+        return '画像の加工中です。しばらくお待ちください。'
