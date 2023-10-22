330ffaf8662ae7e17bd2f52c16a04a079b9164b4
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-23 00:08:10 +0900

    kaisou

--- testapp/views.py
+++ testapp/views.py
@@ -10,51 +10,51 @@ from ultralytics import YOLO
 model = YOLO(model="yolov8n-seg.pt")
 
 # threadingモジュールをインポート
-import threading
+#import threading
 
-processing_done = False
+#processing_done = False
 
-def sakujyo():
+# def sakujyo():
     
-    # 削除したいフォルダのパスを指定
-    folder = 'testapp/static/down'
+#     # 削除したいフォルダのパスを指定
+#     folder = 'testapp/static/down'
 
-    # フォルダが存在するかどうかをチェック
-    if os.path.exists(folder):
-        # フォルダが存在する場合は、shutil.rmtreeで削除
-        shutil.rmtree(folder)
-        # 削除したことを表示
-        print('Folder deleted: ' + folder)
-    else:
-        # フォルダが存在しない場合は、何もしない
-        print('Folder does not exist: ' + folder)
+#     # フォルダが存在するかどうかをチェック
+#     if os.path.exists(folder):
+#         # フォルダが存在する場合は、shutil.rmtreeで削除
+#         shutil.rmtree(folder)
+#         # 削除したことを表示
+#         print('Folder deleted: ' + folder)
+#     else:
+#         # フォルダが存在しない場合は、何もしない
+#         print('Folder does not exist: ' + folder)
 
-    # 削除したいファイルのパターンを指定（.gitignore以外）
-    pattern = '.gitignore'
+#     # 削除したいファイルのパターンを指定（.gitignore以外）
+#     pattern = '.gitignore'
 
-    # testapp/static/upの中のファイルを取得
-    files = os.listdir('testapp/static/up')
+#     # testapp/static/upの中のファイルを取得
+#     files = os.listdir('testapp/static/up')
 
-    # パターンにマッチするファイルを削除
-    for file in files:
-        if file != pattern:
-            os.remove('testapp/static/up/' + file)
-            # 削除したことを表示
-            print('File deleted: ' + file)
+#     # パターンにマッチするファイルを削除
+#     for file in files:
+#         if file != pattern:
+#             os.remove('testapp/static/up/' + file)
+#             # 削除したことを表示
+#             print('File deleted: ' + file)
 
 # 画像処理を行う関数を定義
-def process_image(filename):
-    global processing_done
+#def process_image(filename):
+    #global processing_done
     # 画像を読み込む
     #cv2.imread('testapp/static/up/' + filename)
     # 画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
-    model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
+    #model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
     
-    processing_done = True
+    #processing_done = True
     
-def done_false():
-    global processing_done
-    processing_done = False
+# def done_false():
+#     global processing_done
+#     processing_done = False
 
 # ルートディレクトリにアクセスしたときの処理
 @app.route('/')
@@ -65,7 +65,7 @@ def index():
 # /uploadにPOSTリクエストが送られたときの処理
 @app.route('/upload', methods=['POST'])
 def upload():
-    done_false()
+    #done_false()
     #sakujyo()
     # リクエストからファイルを取得
     file = request.files['file']
@@ -77,8 +77,9 @@ def upload():
     # ファイルを保存
     file.save('testapp/static/up/' + filename)
     # 画像処理を別のスレッドで実行する（非同期）
-    thread = threading.Thread(target=process_image, args=(filename,))
-    thread.start()
+    model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
+    # thread = threading.Thread(target=process_image, args=(filename,))
+    # thread.start()
 
     # # カレントディレクトリのファイルとフォルダの一覧を取得
     # items = os.listdir("./testapp/static")
