8fb4d9c27884ee815147c20938077ab06b1a2f44
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2023-10-23 01:04:29 +0900

    moderu naibu

--- testapp/views.py
+++ testapp/views.py
@@ -6,14 +6,13 @@ import shutil
 # ultralyticsのYOLOモジュールをインポート
 from ultralytics import YOLO
 
-# YOLOv8のセグメンテーションモデルをロード
-model = YOLO(model="yolov8n-seg.pt")
-
 # threadingモジュールをインポート
 import threading
 
 #画像処理を行う関数を定義
 def process_image(filename):
+    # YOLOv8のセグメンテーションモデルをロード
+    model = YOLO(model="yolov8n-seg.pt")
     #画像の加工を行う（ここではYOLOv8でセグメンテーションを行う）
     model.predict(source='testapp/static/up/' + filename, save=True, project='testapp/static/', name="down", exist_ok=True)
 
